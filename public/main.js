d3ef0a028a2ab13a9aa7b3c0bc4331344a99b769
dusanv <dusanv@ccbill.com>
2019-10-24 11:24:21 +0200

    Service worker registration

--- public/main.js
+++ public/main.js
@@ -17,3 +17,15 @@ router.get("/login", req => {
 });
 
 router.init();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
