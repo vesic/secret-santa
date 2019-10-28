176fce625505c15ee486a9611ef3722f687721ee
dusan.vesic <dusan.vesic@gmail.com>
2019-10-28 23:36:28 +0100

    swap register and santa screens

--- public/register.js
+++ public/register.js
@@ -35,3 +35,114 @@
       });
   });
 })();
+
+
+(async () => {
+  if (!"serviceWorker" in navigator) {
+    console.log("Service Worker not supported");
+    return;
+  }
+  const sw = await navigator.serviceWorker.register("/service-worker.js");
+  console.log(sw)
+})();
+
+// if (!("PushManager" in window)) {
+//   throw new Error("No Push API Support!");
+// } else {
+//   console.log("support!");
+//   window.Notification.requestPermission().then(persission => {
+//     if (persission !== "granted") {
+//       throw new Error("Permission not granted!");
+//     }
+//   });
+// }
+
+// test local notifications
+// Notification.requestPermission().then(function(result) {
+//   if(result === 'granted') {
+//     new Notification("Hello", {});
+//   }
+// });
+
+/// =>
+// navigator.serviceWorker.register("service-worker.js");
+
+function urlB64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+navigator.serviceWorker.ready
+  .then(function(registration) {
+    console.log('ready');
+    return registration.pushManager.getSubscription().then(async function(subscription) {
+      if (subscription) {
+        return subscription;
+      }
+
+      const response = await fetch("./vapidPublicKey");
+      const vapidPublicKey = await response.text();
+
+      // const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+      const convertedVapidKey = urlB64ToUint8Array(vapidPublicKey);
+
+      return registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: convertedVapidKey
+      });
+    });
+  })
+  .then(function(subscription) {
+    fetch("./register", {
+      method: "post",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        subscription: subscription
+      })
+    });
+
+    // document.getElementById('doIt').onclick = function() {
+    //   const payload = document.getElementById('notification-payload').value;
+    //   const delay = document.getElementById('notification-delay').value;
+    //   const ttl = document.getElementById('notification-ttl').value;
+
+    //   fetch('./sendNotification', {
+    //     method: 'post',
+    //     headers: {
+    //       'Content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //       subscription: subscription,
+    //       payload: payload,
+    //       delay: delay,
+    //       ttl: ttl,
+    //     }),
+    //   });
+    // };
+
+    // here we have sub
+    setTimeout(() => {
+      fetch("./sendNotification", {
+        method: "post",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          subscription: subscription,
+          payload: 'Payload',
+          delay: '0',
+          ttl: '0'
+        })
+      });
+    }, 0);
+
+  });
+/// =>
---
+++ public/santas.js
@@ -0,0 +1,14 @@
+(async () => {
+  let res = await fetch("/api/santas");
+  let json = await res.json();
+  const table = document.querySelector("table");
+  json.data.map((santa, index) => {
+    const row = table.insertRow();
+    const cell0 = row.insertCell(0);
+    const cell1 = row.insertCell(1);
+    const cell2 = row.insertCell(2);
+    cell0.appendChild(document.createTextNode(index + 1));
+    cell1.appendChild(document.createTextNode(santa.name));
+    cell2.appendChild(document.createTextNode(santa.email));
+  });
+})();
