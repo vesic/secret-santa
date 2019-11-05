e3b4dc19054af0783f1256ac0246340e2b40cc50
Miroslav Gajic <miroslavg@ccbill.com>
2019-11-05 15:30:41 +0100

    caching data and static files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,94 @@
 const CACHE_NAME = "secret-santa-cache-v1";
-const urls = ["index.html", "santas.html"];
+const DATA_CACHE_NAME = 'secret-santa-data-cache-v1';
+
+const FILES_TO_CACHE = [
+  'index.html',
+  'santas.html',
+  'styles.css',
+  'auth.service.js',
+  'install.js',
+  'register.js',
+  'santas.js',
+  'service-worker.js',
+];
 
 self.addEventListener("install", event => {
-  console.log("install...");
-  // // Perform install steps
-  // event.waitUntil(
-  //   caches.open(CACHE_NAME).then(function(cache) {
-  //     console.log("Opened cache");
-  //     return cache.addAll(urls);
-  //   })
-  // );
+
+  console.log("[ServiceWorker] Install");
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("[ServiceWorker] Opened cache");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  // event.respondWith(
-  //   caches.match(event.request).then(function(response) {
-  //     // Cache hit - return response
-  //     if (response) {
-  //       return response;
-  //     }
-  //     return fetch(event.request);
-  //   })
-  // );
+self.addEventListener('activate', (event) => {
+
+  console.log('[ServiceWorker] Activate');
+
+  // Removes previous cached data from disk.
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
+
+  // Network falling back to cache strategy
+  // https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/#network-falling-back-to-cache  
+
+  if (event.request.url.includes('/api/')) {
+    console.log('[Service Worker] Caching DATA', event.request.url);
+    event.respondWith(
+        caches.open(DATA_CACHE_NAME).then((cache) => {
+          console.log('[Service Worker] Caching DATA => cache opened', cache);
+          return fetch(event.request)
+              .then((response) => {
+                // If the response was good, clone it and store it in the cache.
+                if (response.status === 200) {
+                  console.log('[Service Worker] Caching DATA => RESPONSE', response);
+                  cache.put(event.request.url, response.clone());
+                } else {
+                  console.log('[Service Worker] Caching DATA => BAD RESPONSE', response);
+                }
+                return response;
+              }).catch(() => {
+                // Network request failed, try to get it from the cache.
+                console.log('[Service Worker] Caching DATA => FAILED');
+                return cache.match(event.request);
+              });
+        }));
+    return;
+  }
+
+  event.respondWith(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log(`[Service Worker] STATIC page request:`, event.request)
+        return cache.match(event.request)
+            .then((response) => {
+              if (response) {
+                console.log(`[Service Worker] STATIC page *** Returned response from cache for ${event.request.url} ***`);
+              } else {
+                console.log(`[Service Worker] STATIC page fetching ${event.request.url}`);
+              }
+              return response || fetch(event.request);
+            });
+      })
+  );
 });
 
 self.addEventListener("push", function(event) {
